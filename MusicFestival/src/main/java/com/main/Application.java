d751e2ebd303bbfbd3047a42b6e1ab4f2122c495
Reshmavysakh <33819149+Reshmavysakh@users.noreply.github.com>
2019-10-29 11:18:39 +1100

    Add files via upload

--- MusicFestival/src/main/java/com/main/Application.java
+++ MusicFestival/src/main/java/com/main/Application.java
@@ -2,15 +2,18 @@ package com.main;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 
 public class Application {
 
@@ -43,75 +46,96 @@ public class Application {
 				while (sc.hasNext()) {
 					jsonArray += sc.nextLine();
 				}
-				System.out.println("\nJSON Response in String format");
-				System.out.println(jsonArray);
+//				System.out.println("\nJSON Response in String format");
+//				System.out.println(jsonArray);
 				// Close the stream when reading the data has been finished
 				sc.close();
 			}
 
-			JSONArray ob = new JSONArray(jsonArray);
-			Map<String, String> bandRecord=new HashMap<>();
-			Map<String, String> festivalBand=new HashMap<>();
-			String rootObject ;
+			JSONArray ob;
+			Map<String, String> bandRecord;
+			Map<String, String> festivalBand;
+			String rootObject;
 			JSONArray firstArray;
-			JSONObject jb ;
+			JSONObject jb;
 			JSONObject jsobj;
-			
-			
-			for (int i = 0; i < ob.length(); i++) {
-				 jsobj = ob.getJSONObject(i);
+			List<String> bands;
+			List<String> musicfestivals;
+			List<Object> records;
+			if (jsonArray.length() > 0) {
+				ob = new JSONArray(jsonArray);
+				for (int i = 0; i < ob.length(); i++) {
+					jsobj = ob.getJSONObject(i);
 
-				 rootObject = (String) jsobj.get("name");
-				 firstArray = (JSONArray) jsobj.get("bands");
-				
-				if (rootObject.equals(null) | rootObject == " ")
-				{
-					
+					rootObject = (String) jsobj.get("name");
+					firstArray = (JSONArray) jsobj.get("bands");
+
+					if (rootObject.equals(null) | rootObject == " ") {
+
+					}
+					for (int x = 0; x < firstArray.length(); x++) {
+						if (firstArray.getJSONObject(x).equals(null)) {
+							jb = null;
+						} else
+							jb = firstArray.getJSONObject(x);
+
+						String bandname = jb.getString("name");
+						String record = jb.getString("recordLabel");
+						bandRecord = new HashMap<String, String>();
+						festivalBand = new HashMap<String, String>();
+						bandRecord.put(bandname, record);
+						festivalBand.put(rootObject, bandname);
+
+						Map<Object, Object> sortedbandrecord = bandRecord.entrySet().stream()
+								.sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey,
+										Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+						Map<Object, Object> sortedfestivalband = festivalBand.entrySet().stream()
+								.sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey,
+										Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+						String recod_label = "";
+						records = new ArrayList<Object>();
+						bands = new ArrayList<String>();
+						musicfestivals = new ArrayList<String>();
+						for (Map.Entry<Object, Object> entry : sortedbandrecord.entrySet()) {
+							if ((records.size()) > 0 && records.contains(entry.getValue())) {
+								// do nothing
+							} else {
+								records.add(entry.getValue());
+								recod_label = entry.getValue().toString();
+
+								System.out.println("RecordLabel    :" + recod_label);
+							}
+
+							for (Map.Entry<Object, Object> entry1 : sortedfestivalband.entrySet()) {
+								if (entry1.getValue().equals(entry.getKey())) {
+									bands.add(entry1.getValue().toString());
+									musicfestivals.add(entry1.getKey().toString());
+								}
+							}
+							if (!(bands.isEmpty())) {
+								bands.forEach(band -> {
+									System.out.println("Band:" + band);
+								});
+							}
+							if (!(musicfestivals.isEmpty())) {
+								musicfestivals.forEach(festival -> {
+									System.out.println("Music Festivals:" + festival);
+								});
+							}
+						}
+
+					}
 				}
-				 for (int x = 0; x < firstArray.length(); x++) {
-					 if(firstArray.getJSONObject(x).equals(null))
-							 {
-						 jb=null;
-							 }
-					 else
-			             jb = firstArray.getJSONObject(x);
-					 
-			            String bandname = jb.getString("name");
-			            String record = jb.getString("recordLabel");
-			            bandRecord.put(bandname, record);
-			            festivalBand.put(bandname,rootObject);
-			           
-			        }
+			} else {
+				System.out.println("No input received");
 			}
-
-			 Map<Object, Object> sortedbandrecord = bandRecord.entrySet().stream()
-		                .sorted(Map.Entry.comparingByValue())
-		                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-		                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-			 Map<Object, Object> sortedfestivalband = festivalBand.entrySet().stream()
-		                .sorted(Map.Entry.comparingByValue())
-		                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-		                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-			 
-			 for (Map.Entry<Object, Object> entry : sortedbandrecord.entrySet()) {
-			        		System.out.println( "RecordLabel    :"+ entry.getValue());
-			        for (Map.Entry<Object, Object> entry1 : sortedfestivalband.entrySet()) {
-			        	if(entry.getKey().equals(entry1.getKey()))
-			        	{
-			        		System.out.println("Bands    :"+entry1.getKey());
-				        System.out.println("Music Festivals :"+ entry1.getValue());
-			        	}
-			        	
-			    }
-			 }
 			// Disconnect the HttpURLConnection stream
 			conn.disconnect();
 		} catch (Exception e) {
 			e.printStackTrace();
-			
+
 		}
 	}
 
 }
-
